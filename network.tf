49ee69c078aa521598571e3c0b055c81f4ab93cd
vilorij <vilorij@ya.ru>
2023-02-27 19:48:56 +0700

    move from deprecated

--- network.tf
+++ network.tf
@@ -1,11 +1,12 @@
+data aws_instance wireguard_interface {
+  instance_id = aws_instance.wireguard.id
+}
+
 resource "aws_route" "to_wireguard_middlebox" {
-  count                  = var.create_routes ? length(data.aws_route_tables.rts.ids) : 0
-  route_table_id         = tolist(data.aws_route_tables.rts.ids)[count.index]
-  destination_cidr_block = var.wg_client_cidr
-  instance_id            = aws_instance.wireguard.id
-  lifecycle {
-    ignore_changes = [network_interface_id]
-  }
+  count                   = var.create_routes ? length(data.aws_route_tables.rts.ids) : 0
+  route_table_id          = tolist(data.aws_route_tables.rts.ids)[count.index]
+  destination_cidr_block  = var.wg_client_cidr
+  network_interface_id    = data.aws_instance.wireguard_interface.network_interface_id
 }
 
 data "aws_route_tables" "rts" {
