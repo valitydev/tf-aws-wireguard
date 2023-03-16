4af22250dafb95d09c653d77ba0aae00bdb0cc90
AydarN <9845662+AydarN@users.noreply.github.com>
2023-03-16 12:11:55 +0300

    rmv-unnecessary-ssh

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "wireguard" {
   name        = "wireguard-${var.aws_region}"
-  description = "Wireguard server. In: 22/tcp, ${var.wg_server_port}/udp; Out: any."
+  description = "Wireguard server. In: ${var.wg_server_port}/udp; Out: any."
   vpc_id      = var.vpc_id
   tags        = var.aws_tags
 
@@ -8,13 +8,6 @@ resource "aws_security_group" "wireguard" {
     from_port   = var.wg_server_port
     to_port     = var.wg_server_port
     protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
